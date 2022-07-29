beef6d90643b41c4d6a6d59d5f8875f8cffe0160
Edgar Narvaez <enarvaez_19@alu.uabcs.mx>
2022-07-29 10:09:41 -0600

    Initial Commit

---
+++ AppMueblesVenta/AppMueblesVentaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppMueblesVentaApp.swift
+//  AppMueblesVenta
+//
+//  Created by Edgar Narvaez on 29/07/22.
+//
+
+import SwiftUI
+
+@main
+struct AppMueblesVentaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
